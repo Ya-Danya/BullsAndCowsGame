45541c4d784af54b3a10b405f50c03e4234f83c1
Ya-Danya <ddsamsonenko@edu.hse.ru>
2023-10-17 09:20:45 +0300

    Zadolbalsa

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,7 @@
 let bulls;
 
-function printAns(arr) {
-  cowOrBull(arr[0], document.getElementById('first_out'));
-  cowOrBull(arr[1], document.getElementById('second_out'));
-  cowOrBull(arr[2], document.getElementById('third_out'));
-  cowOrBull(arr[3], document.getElementById('fourth_out'));
-}
-
+// На вход подается число - идентификатор быка, коровы или
+// иного числа и элемент в котором мы будем менять содержимое.
 function cowOrBull(code, element) {
   if (code == 1) {
     element.src = "svgs/cow.svg";
@@ -17,7 +12,38 @@ function cowOrBull(code, element) {
   }
 }
 
+// Функция выводит значения массива в виде иконок коров и быков.
+// По умолчанию и при значении 0 выводит пустоту.
+// Если значение == 1, то корову.
+// Если значение == 2, то быка.
+function printAns(arr) {
+  cowOrBull(arr[0], document.getElementById('first_out'));
+  cowOrBull(arr[1], document.getElementById('second_out'));
+  cowOrBull(arr[2], document.getElementById('third_out'));
+  cowOrBull(arr[3], document.getElementById('fourth_out'));
+}
 
+function printComputerTry(number) {
+  let output_div = document.getElementById('output');
+
+  output_div.insertAdjacentHTML("beforeend",
+  `
+  <div class="">
+    <div>${number[0]}</div>
+  </div>
+  <div class="">
+    <div>${number[1]}</div>
+  </div>
+  <div class="">
+    <div>${number[2]}</div>
+  </div>
+  <div class="">
+    <div>${number[3]}</div>
+  </div>
+  `);
+}
+
+// Генерация рандомного числа из 4 неповторяющихся цифр.
 function generateNumber() {
   let bulls = [-1, -1, -1, -1];
 
@@ -38,7 +64,8 @@ function generateNumber() {
   return bulls;
 }
 
-function compare() {
+// Функция сравнения загаданного числа и числа, которое ввел пользователь.
+function humanCompare() {
   let input_num = new Array(4);
 
   let chat = document.getElementById('chat');
@@ -65,8 +92,59 @@ function compare() {
     return;
   }
 
+  let ans = [0, 0, 0, 0];
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (Number(input_num[i]) == Number(bulls[j])) {
+        if (i == j) {
+          ans[i] = 2;
+        } else {
+          ans[i] = 1;
+        }
+        break;
+      }
+    }
+  }
+  chat.textContent = 'Введите число.'
+  
+
+  return(ans);
+}
 
 
+
+function inputNumber() {
+  let input_num = new Array(4);
+
+  let chat = document.getElementById('chat');
+
+  input_num[0] = document.getElementById('first_num').value;
+  if (Number(input_num[0]) < 0 || Number(input_num[0]) > 9) {
+    chat.textContent = 'А ну-ка внимательнее!\n Ввод некорректен.'
+    return;
+  }
+
+  input_num[1] = document.getElementById('second_num').value;
+  if (Number(input_num[1]) < 0 || Number(input_num[0]) > 9) {
+    chat.textContent = 'А ну-ка внимательнее!\n Ввод некорректен.'
+    return;
+  }
+  input_num[2] = document.getElementById('third_num').value;
+  if (Number(input_num[2]) < 0 || Number(input_num[0]) > 9) {
+    chat.textContent = 'А ну-ка внимательнее!\n Ввод некорректен.'
+    return;
+  }
+  input_num[3] = document.getElementById('fourth_num').value;
+  if (Number(input_num[3]) < 0 || Number(input_num[0]) > 9) {
+    chat.textContent = 'А ну-ка внимательнее!\n Ввод некорректен.'
+    return;
+  }
+
+  return input_num;
+}
+
+function compare(input_num) {
   let ans = [0, 0, 0, 0];
 
   for (let i = 0; i < 4; i++) {
@@ -83,21 +161,37 @@ function compare() {
   }
   chat.textContent = 'Введите число.'
   console.log(ans);
-  printAns(ans);
 
   return(ans);
 }
 
-function bullsNCows (secret) {
+// В данной функции вызывается печать сравнения пользовательского ввода и
+// загаданного числа в режиме угадывания пользователем числа.
+function humanPlayer() {
+  let input = inputNumber();
+  let com = compare(input);
+  printAns(com);
+  let chat = document.getElementById('chat');
+  if ((com[0] == 2) && (com[1] == 2) && (com[2] == 2) && (com[0] == 3)) {
+    generateNumber();
+    chat.textContent = 'Победа! Можете сыграть заново.'
+  }
+}
+
+// Описание функции, в которой компьютер угадывает загаданное пользователем число
+function bullsNCows () {
+  let input = inputNumber();
+  bulls = input;
+  console.info("Загано число " + bulls);
+  // Массив в который записываются попытки угадывания.
   let tries = new Array();
+  // Массив, в который записываются результаты сравнения попытки и ответа.
   let compares = new Array();
   
-  //List<int[]> tries = new ArrayList<int[]>();
-  //ArrayList<int[]> compares = new ArrayList<int[]>();
-  let ans = [-1, -1, -1, -1][-1, -1, -1, -1];
+  let ans = [-1, -1, -1, -1];
   
   let cows = new Array(4);
-  //ArrayList<Integer>[] cows = new ArrayList[4];
+  
 
   let bulls_amount = 0;
   let cows_amount = 0;
@@ -111,13 +205,15 @@ function bullsNCows (secret) {
       ans[i] = tries[0][i];
       bulls_amount++;
     } else if (compares[0][i] == 1) {
+      cows[cows_amount] = new Array();
       cows[cows_amount].push(tries[0][i]);
       cows[cows_amount].push(i);
       cows_amount++;
     }
   }
+  printComputerTry(tries[0]);
   console.info("Первая попытка: " + compares[0]);
-
+  console.info(cows);
   if (bulls_amount == 4) {
     console.info(ans);
     return;
@@ -132,41 +228,36 @@ function bullsNCows (secret) {
     if (compares[1][i] == 2) {
       ans[i] = tries[1][i];
       bulls_amount++;
-    } else if (compares[0][i] == 1) {
+    } else if (compares[1][i] == 1) {
+      cows[cows_amount] = new Array();
       cows[cows_amount].push(tries[1][i]);
       cows[cows_amount].push(i);
       cows_amount++;
     }
   }
+  printComputerTry(tries[1]);
   console.info("Вторая попытка: " + compares[1]);
-
+  console.info(cows);
   if (bulls_amount == 4) {
     console.info(ans);
     return;
   }
 
+
+
   // Третяя попытка угадывания.
-  tries.push([-1, -1, -1, -1]);
+  tries.push(ans);
+  console.info("Кол-во коров" + cows_amount);
+  console.info("Кол-во быков" + bulls_amount);
+
+  
   // Расставляем цифры учитывая, что у нас 2 коровы
-  if (cows_amount == 2) {
-    if (cows[1][1] != cows[0][1]) {
-      tries[2][cows[1][1]] = cows[0][0];
-      cows[0].push[cows[1][1]];
-      tries[2][cows[0][1]] = cows[1][0];
-      cows[1].push[cows[0][1]];
-    } else {
-      //
-      for (let i = 0; i < 4; ++i) {
-        if ((cows[0][1] != i)) {
-          tries[2][i] = cows[0][0];
-          cows[0].push[i];
-          for (let j = i; j < 4; ++j) {
-            if ((cows[1][1] != i)) {
-              tries[2][i] = cows[1][0];
-              cows[1].push[i];
-              break;
-            }
-          }
+  if (cows_amount + bulls_amount == 2) {
+    for (let i = 0; i < cows_amount; ++i) {
+      for (let j = 0; j < 4; ++j) {
+        if ((tries[2][j] == -1) && (j != cows[i][1])) {
+          tries[2][j] = cows[i][0];
+          cows[i].push(j);
           break;
         }
       }
@@ -175,6 +266,7 @@ function bullsNCows (secret) {
     let cow_num = 8;
     for (let i = 0; i < 4; i++) {
       if (tries[2][i] == -1) {
+        console.info(cow_num);
         tries[2][i] = cow_num++;
         cows[cows_amount] = new Array();
         cows[cows_amount].push(cow_num);
@@ -183,48 +275,119 @@ function bullsNCows (secret) {
       }
     }
   // Собираем комбинацию для трех коров
-  } else if (cows_amount == 3) {
-    let except_cow_number = -1;
-    if (cows[0][1] == cows[1][1]) {
-      tries[2][cows[0][1]] = cows[2][0];
-      cows[2].push(cows[0][1]);
-      except_cow_number = 2;
-    } else if (cows[0][1] == cows[2][1]) {
-      tries[2][cows[0][1]] = cows[1][0];
-      cows[1].push(cows[0][1])
-      except_cow_number = 1;
-    } else if (cows[1][1] == cows[2][1]) {
-      tries[2][cows[1][1]] = cows[0][0];
-      cows[0].push(cows[0][1]);
-      except_cow_number = 0;
-    } 
-    for (let i = 0; i < 3; ++i) {
-      if (except_cow_number == i) {
-        break;
-      } else {
-        for (let j = 0; j < 4; ++j) {
-          if ((tries[2][j] != -1) && (cows[i][1] != j)) {
-            tries[2][j] = cows[i][0];
-            cows[i].push(j);
-            break;
-          }
-        }
-      }
-      for (let i = 0; i < 4; ++i) {
-        if (tries[2][i] != 0) {
-          tries[2][i] = 8;
+  } else if (cows_amount + bulls_amount == 3) {
+    for (let i = 0; i < cows_amount; ++i) {
+      for (let j = 0; j < 4; ++j) {
+        if ((tries[2][j] == -1) && (j != cows[i][1])) {
+          tries[2][j] = cows[i][0];
+          cows[i].push(j);
+          break;
         }
       }
     }
 
-  } else if (cows_amount == 4) {
     for (let i = 0; i < 4; ++i) {
+      if (tries[2][i] == -1) {
+        tries[2][i] = 8;
+      }
+    }
+  } else if (cows_amount + bulls_amount == 4) {
+    for (let i = 0; i < cows_amount; ++i) {
       for (let j = 0; j < 4; ++j) {
-        if ((tries != -1)) {
+        if ((tries[2][j] == -1) && (j != cows[i][1])) {
           tries[2][j] = cows[i][0];
+          cows[i].push(j);
+          break;
         }
       }
     }
+  }
+
+  compares.push(compare(tries[2]));
+
+  for (let i = 0; i < 4; ++i) {
+    if (compares[2][i] == 2) {
+      ans[i] = tries[2][i];
+      bulls_amount++;
+    }
+  }
+
+  if (bulls_amount + cows_amount == 3) {
+    for (let i = 0; i < 4; ++i) {
+
+    }
+  } 
+
+  
+
+  printComputerTry(tries[2]);
+  console.info("Третяя попытка: " + compares[2]);
+  console.info(cows);
+  if (bulls_amount == 4) {
+    console.info(ans);
+    return;
+  }
+
+
+  // Формирование 4 попытки я задолбался и хочу спать)
+
+
+  tries.push(ans);
+
+  if (cows_amount + bulls_amount == 3) {
+    for (let i = 0; i < 4; ++i) {
+      
+    }
+  } else {
+    tries[2];
+    for (let i = 0; i < 4 - bulls_amount; ++i) {
+      for (let j = 0; j < 4; ++j) {
+        if ((tries[2][j] != ans[0]) && (tries[2][j] != ans[1]) && (tries[2][j] != ans[2]) && (tries[2][j] != ans[3])) {
+          let k = j + 1;
+          while (k % 4 == j) {
+            if (ans[k] == -1 ) {
+              ans[k] = tries[2][j];
+            }
+          }
+        }
+      }
+    }
+
+
+    let tempo = new Array();
+    for (let i = 0; i < 4; ++i) {
+      if (ans[i] != -1) {
+        tempo.push(ans[i]);
+      }
+    }
+
+  }
+
+  // Пятая попытка.
+  tries.push(tries[3]);
+  tries[4].unshift(tries[4].pop());
+
+  compares.push(compare(tries[3]));
+
+  for (let i = 0; i < 4; ++i) {
+    if (compares[4][i] == 2) {
+      ans[i] = tries[4][i];
+      bulls_amount++;
+    }
+  }
+  for (let i = 0; i < 4; ++i) {
+    if (tries[4][i] == -1) {
+      tries[4][i] = ans[i];
+    }
+  }
+}
+
+function switchCheckBox() {
+  let checkbox = document.getElementById('switcher');
+  if (checkbox.checked) {
+    document.getElementById('input_numb').onclick = bullsNCows;
+  } else {
+    document.getElementById('input_numb').onclick = humanPlayer;
   }
 }
 
@@ -232,7 +395,8 @@ function bullsNCows (secret) {
 
 window.onload = function () {
   bulls = generateNumber();
-  console.info(bulls);
 
-  document.getElementById('input_numb').onclick = compare;
+  console.info(bulls);
+  document.getElementById('switcher').onclick = switchCheckBox;
+  document.getElementById('input_numb').onclick = humanPlayer;
 };
